80887927dd00a19ecaf42d293ccae3ada2ab3113
JMONTANEZ <jordinym@gmail.com>
2017-02-26 18:36:32 -0500

    Implementando repositorio clase 26 02 2017

---
+++ Tibox.Repository.Tests/CustomerRepositoryTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Tibox.Repository;
+using Tibox.Models;
+
+namespace Tibox.DataAccess.Tests
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        private readonly IRepository<Customer>  _repository;
+
+        public CustomerRepositoryTest()
+        {
+            _repository = new BaseRepository<Customer>();
+        }
+        [TestMethod]
+        public void Get_All_Customers()
+        {
+            var result = _repository.GetAll();
+            Assert.AreEqual(result.Count() > 0, true);
+        }
+
+        [TestMethod]
+        public void Insert_Customer()
+        {
+            var customer = new Customer
+            {
+                FirstName = "Julio",
+                LastName = "Velarde",
+                City = "Huancavelica",
+                Country = "Peru",
+                Phone="155156156"
+            };
+
+            var result = _repository.Insert(customer);
+            Assert.AreEqual(result > 0, true);
+        }
+        [TestMethod]
+        public void Update_Customer()
+        {
+            Customer customer = _repository.GetEntityById(91);
+
+            Assert.AreEqual(customer != null, true);
+            
+            Assert.AreEqual(_repository.Update(customer), true);
+        }
+
+        [TestMethod]
+        public void Delete_Customer()
+        {
+          
+            Customer customer = _repository.GetEntityById(92);
+            Assert.AreEqual(customer != null, true);
+
+            Assert.AreEqual(_repository.Delete(customer), true);
+        }
+
+        [TestMethod]
+        public void Get_Customer_By_Id()
+        {
+            
+            var result = _repository.GetEntityById(91);
+            Assert.AreEqual(result !=null, true);
+        }
+    }
+}
---
+++ Tibox.Repository/BaseRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tibox.Models;
+
+namespace Tibox.Repository
+{
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        private readonly string _connectionString;
+
+        public BaseRepository()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["NorthwindConnectionString"].ConnectionString;
+        }
+
+        public bool Delete(T entity)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+
+                return connection.Delete<T>(entity);
+            }
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+
+                return connection.GetAll<T>();
+            }
+        }
+
+        public T GetEntityById(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+
+                return connection.Get<T>(id);
+            }
+        }
+
+        public int Insert(T entity)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                //tambien permite la insercion en bloque con la misma sintaxis
+                return (int)connection.Insert<T>(entity);
+            }
+        }
+
+        public bool Update(T entity)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+
+                return connection.Update<T>(entity);
+            }
+        }
+    }
+}
