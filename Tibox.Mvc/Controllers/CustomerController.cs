f4179cf0259fe747ba994886b7f5858b2b8ecb3e
jmontanez <jordini.montanez@tibox.com.pe>
2017-03-13 16:26:33 -0500

    proyecto mvc

---
+++ Tibox.Mvc/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tibox.Models;
+using Tibox.UnitOfWork;
+
+namespace Tibox.Mvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly TiboxUnitOfWork _unit;
+        public CustomerController()
+        {
+            _unit = new TiboxUnitOfWork();
+        }
+        // GET: Customer
+        public ActionResult Index()
+        { 
+            return View(_unit.Customers.GetAll());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {   
+            var id=    _unit.Customers.Insert(customer);
+            if (id > 0) return RedirectToAction("Index");
+            return View(customer);
+        }
+    }
+}
